ae33ae95fb3978562fdb9abb3334d67536a43ea8
porkbeans <me@porkbeans.dev>
2025-03-13 01:43:25 +0900

    Add labels to images

--- common/python.mk
+++ common/python.mk
@@ -12,6 +12,8 @@ build:
 	docker buildx build \
 		--tag "$(TAG)" \
 		--build-arg "PYTHON_VERSION=$(PYTHON_VERSION)" \
+		--label "org.opencontainers.image.source=https://github.com/porkbeans/container-images" \
+		--label "org.opencontainers.image.revision=$(shell git rev-parse HEAD)" \
 		--debug \
 		--progress plain \
 		--load .
@@ -27,6 +29,8 @@ push:
 	docker buildx build \
 		--tag "$(TAG)" \
 		--build-arg "PYTHON_VERSION=$(PYTHON_VERSION)" \
+		--label "org.opencontainers.image.source=https://github.com/porkbeans/container-images" \
+		--label "org.opencontainers.image.revision=$(shell git rev-parse HEAD)" \
 		--platform linux/amd64,linux/arm64 \
 		--provenance=false \
 		--push .
